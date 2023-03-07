73cdd604daf53840bf043debc6b3b134531a4d00
KavyaChopra <76686084+KavyaChopra@users.noreply.github.com>
2023-03-07 17:44:18 +0000

    test: add file to test connection between Excel and Python component

---
+++ sydx/sydx-master/Python/src/main/outgoing.py
@@ -0,0 +1,18 @@
+import sys, os
+# sys.path.append(os.path.abspath('../main'))
+
+import sydx
+
+import numpy as np
+
+
+sydx.port(8001)
+sydx.connect("localhost", 8000)
+
+print("BIRTCH")
+# Python object you want to send
+data = np.array("hello", "world")
+
+sydx.put('first', 1)
+
+sydx.put('list', data)
