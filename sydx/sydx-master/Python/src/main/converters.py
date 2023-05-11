271539840690125b98cff529de93a92fcae74d77
KavyaChopra <76686084+KavyaChopra@users.noreply.github.com>
2023-05-11 03:14:00 +0100

    feat: changed serialization to return value type for all datatypes

--- sydx/sydx-master/Python/src/main/converters.py
+++ sydx/sydx-master/Python/src/main/converters.py
@@ -7,12 +7,12 @@ import sydx
 def numpy_numeral_to_json_object(obj):
     if isinstance(obj, np.int32):
         return {
-            'type': 'int32',
+            'value_type': 'int32',
             'value': int(obj)
         }
     elif isinstance(obj, np.float64):
         return {
-            'type': 'float64',
+            'value_type': 'float64',
             'value': float(obj)
         }
     else:
@@ -20,9 +20,9 @@ def numpy_numeral_to_json_object(obj):
 
 def numpy_numeral_from_json_object(json_object):
     if isinstance(json_object, dict):
-        if 'type' in json_object and json_object['type'] == 'int32':
+        if 'value_type' in json_object and json_object['value_type'] == 'int32':
             return np.int32(json_object['value'])
-        elif 'type' in json_object and json_object['type'] == 'float64':
+        elif 'value_type' in json_object and json_object['value_type'] == 'float64':
             return np.float64(json_object['value'])
         else:
             raise ValueError('Unable to deserialise an object')
@@ -51,14 +51,14 @@ def numpy_array_to_json_object(obj):
     if not isinstance(obj, np.ndarray):
         raise ValueError('Unable to serialise an object')
     return {
-        'type': 'array',
-        'values': __numpy_array_to_list(obj)
+        'value_type': 'array',
+        'value': __numpy_array_to_list(obj)
     }
 
 def numpy_array_from_json_object(json_object):
-    if isinstance(json_object, dict):
-        if 'type' in json_object and json_object['type'] == 'array':
-            return __list_to_numpy_array(json_object['values'])
+    if isinstance(json_object, list): 
+        if 'value_type' in json_object and json_object['value_type'] == 'array':
+            return __list_to_numpy_array(json_object['value'])
         else:
             raise ValueError('Unable to deserialise an object')
     raise ValueError('Unable to deserialise an object')
@@ -66,21 +66,42 @@ def numpy_array_from_json_object(json_object):
 def plain_old_data_to_json_object(obj):
     logger = logging.getLogger()
     logger.debug('Attempting to serialise a plain old data object')
-    if isinstance(obj, (int, float, complex)):
-        return obj
+    if isinstance(obj, int):
+        return {
+            'value_type': 'int',
+            'value': obj
+        }
+    elif isinstance(obj, float):
+        return {
+            'value_type': 'float',
+            'value': obj
+        } 
     elif isinstance(obj, str):
-        return obj
+        return {
+            'value_type': 'string',
+            'value': obj
+        }
     elif isinstance(obj, list):
-        return [sydx.__serialise(x) for x in obj]
+        return {
+            'value_type': 'list',
+            'value': [sydx.__serialise(x) for x in obj]
+        }
+    # if isinstance(obj, (int, float, complex)):
+    #     return obj
+    # elif isinstance(obj, str):
+    #     return obj
+    # elif isinstance(obj, list):
+    #     return [sydx.__serialise(x) for x in obj]
     elif isinstance(obj, tuple):
         return {
-            'type': 'tuple',
-            'values': list(obj)
+            'value_type': 'tuple',
+            'value': [sydx.__serialise(x) for x in list(obj)]
+            # 'value': list(obj)
         }
     elif isinstance(obj, dict):
         return {
-            'type': 'dict',
-            'values': {sydx.__serialise(x): sydx.__serialise(y) for x, y in obj.items()}
+            'value_type': 'dict',
+            'value': {x: sydx.__serialise(y) for x, y in obj.items()}
         }
     elif obj is None:
         return None
@@ -91,20 +112,28 @@ def plain_old_data_to_json_object(obj):
 def plain_old_data_from_json_object(json_object):
     logger = logging.getLogger()
     logger.debug('Attempting to deserialise a plain old data object')
-    if isinstance(json_object, (int, float, complex)):
-        return json_object
-    elif isinstance(json_object, str):
-        return json_object
-    elif isinstance(json_object, list):
-        return [sydx.__deserialise(x) for x in json_object]
-    elif isinstance(json_object, tuple):
-        return tuple([sydx.__deserialise(x) for x in json_object])
-    elif isinstance(json_object, dict):
-        if 'type' in json_object:
-            if json_object['type'] == 'dict':
-                return {sydx.__deserialise(x): sydx.__deserialise(y) for x, y in json_object['values'].items()}
-            elif json_object['type'] == 'tuple':
-                return tuple([sydx.__deserialise(x) for x in json_object['values']])
+    # if isinstance(json_object, (int, float, complex)):
+    #     return json_object
+    # elif isinstance(json_object, str):
+    #     return json_object
+    # elif isinstance(json_object, list):
+    #     return [sydx.__deserialise(x) for x in json_object]
+    # elif isinstance(json_object, tuple):
+    #     return tuple([sydx.__deserialise(x) for x in json_object])
+    if isinstance(json_object, dict):
+        if 'value_type' in json_object:
+            if json_object['value_type'] == 'int':
+                return json_object['value']
+            elif json_object['value_type'] == 'float':
+                return json_object['value']
+            elif json_object['value_type'] == 'string':
+                return json_object['value']
+            elif json_object['value_type'] == 'list':
+                return [sydx.__deserialise(x) for x in json_object['value']]
+            elif json_object['value_type'] == 'tuple':
+                return tuple([sydx.__deserialise(x) for x in json_object['value']])
+            elif json_object['value_type'] == 'dict':
+                return {x: sydx.__deserialise(y) for x, y in json_object['value'].items()}
         else:
             logger.debug('This JSON object does not seem to represent a plain old data object')
             raise ValueError('Unable to deserialise an object')
