4fc2ef436b254ccdd87873711d0fafe6ec6677ff
kc2320 <kc2320@ic.ac.uk>
2023-05-30 18:02:27 +0100

    feat: write main excel custom functions file

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -6,8 +6,6 @@ import { serialize } from "./zend/converters";
 import { PutRequest } from "./zend/types";
 import { Connection } from "./zend/connection";
 
-console.log("fuck");
-
 // global variables
 let storage: Storage = new Storage();
 let connections: Connections = new Connections();
@@ -79,60 +77,60 @@ export function logMessage(message: string): string {
   return message;
 }
 
-// /**
-//  * Puts a value into global storage and sends value to other frameworks.
-//  * @customfunction PUT
-//  * @param name the key for value.
-//  * @param value the value to put in storage.
-//  * @returns string respresenting success of putting the value.
-//  */
-// export async function put(name: string, value: any): Promise<string> {
-//   storage.put(name, value);
-//   connections.sendToAll({ request_type: "PUT_REQUEST", name: name, value: serialize(value) } as PutRequest);
-//   return "success";
-// }
+/**
+ * Puts a value into global storage and sends value to other frameworks.
+ * @customfunction PUT
+ * @param name the key for value.
+ * @param value the value to put in storage.
+ * @returns string respresenting success of putting the value.
+ */
+export async function put(name: string, value: any): Promise<string> {
+  storage.put(name, value);
+  connections.sendToAll({ request_type: "PUT_REQUEST", name: name, value: serialize(value) } as PutRequest);
+  return "success";
+}
 
-// /**
-//  * Gets a value from the global storage.
-//  * @customfunction GET
-//  * @param name the key to get the value of from storage.
-//  * @returns value from storage corresponding to given key.
-//  */
-// export function get(name: string): any {
-//   return storage.get(name);
-// }
+/**
+ * Gets a value from the global storage.
+ * @customfunction GET
+ * @param name the key to get the value of from storage.
+ * @returns value from storage corresponding to given key.
+ */
+export function get(name: string): any {
+  return storage.get(name);
+}
 
-// /**
-//  * Opens a server port for incoming connections from other frameworks.
-//  * @customfunction OPEN_PORT
-//  * @param port port number to open server on and listen for connections
-//  * @param host host name for server; default is localhost
-//  * @returns string indicating success of opening port
-//  */
-// export async function openPort(port: number, host: string = "localhost"): Promise<string> {
-//   if (serverPort === null) {
-//     server = new ZendServer(port, host, storage, connections);
-//     serverPort = port;
-//     server.listen();
-//     return "Listening on {port}";
-//   } else {
-//     return "Port already open on {serverPort}";
-//   }
-// }
+/**
+ * Opens a server port for incoming connections from other frameworks.
+ * @customfunction OPEN_PORT
+ * @param port port number to open server on and listen for connections
+ * @param host host name for server; default is localhost
+ * @returns string indicating success of opening port
+ */
+export async function openPort(port: number, host: string = "localhost"): Promise<string> {
+  if (serverPort === null) {
+    server = new ZendServer(port, host, storage, connections);
+    serverPort = port;
+    server.listen();
+    return "Listening on {port}";
+  } else {
+    return "Port already open on {serverPort}";
+  }
+}
 
-// /**
-//  * Connects to a server at given port and hostname for communication.
-//  * @customfunction CONNECT
-//  * @param port port number to connect to
-//  * @param host host name to connect to; default is localhost
-//  * @returns the string handle for the client that denotes the connection
-//  */
-// export async function connect(host: string = "localhost", port: number): Promise<string> {
-//   if (serverPort === null) {
-//     return "Must open port before connecting";
-//   }
-//   return (await _connect(host, port)).getHandle();
-// }
+/**
+ * Connects to a server at given port and hostname for communication.
+ * @customfunction CONNECT
+ * @param port port number to connect to
+ * @param host host name to connect to; default is localhost
+ * @returns the string handle for the client that denotes the connection
+ */
+export async function connect(host: string = "localhost", port: number): Promise<string> {
+  if (serverPort === null) {
+    return "Must open port before connecting";
+  }
+  return (await _connect(host, port)).getHandle();
+}
 
 // Global function; not an Excel function
 export async function _connect(host: string, localPort: number): Promise<ZendClient> {
